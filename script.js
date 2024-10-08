ca3cb7a348bad163fba310c451f7c994b7f021d9
EladReuveny <126913072+EladReuveny@users.noreply.github.com>
2024-10-08 20:56:06 +0300

    2nd commit

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const seeDetails = document.querySelector('.pots__header > span');
-const pots = document.querySelector('.pots');
+const seeDetails = document.querySelector(".pots__header > span");
+const pots = document.querySelector(".pots");
 
-seeDetails.addEventListener('click', () => {
-  pots.classList.toggle('non-expanded'); 
+seeDetails.addEventListener("click", () => {
+  pots.classList.toggle("non-expanded");
+});
+
+const viewAll = document.querySelector(".transactions__header > span");
+const transactions = document.querySelector(".transactions");
+
+viewAll.addEventListener("click", () => {
+  transactions.classList.toggle("non-expanded");
+});
+
+const seeDetails2 = document.querySelector(".recurring-bills__header > span");
+const recurringBills = document.querySelector(".recurring-bills");
+
+seeDetails2.addEventListener("click", () => {
+  recurringBills.classList.toggle("non-expanded");
 });
